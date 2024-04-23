d22779c62c4065638eec85066b6677c315a1d404
Nikola0707 <ristoski.nikola@gmail.com>
2024-04-23 20:59:57 +0200

    Objects, string and number methods and arrays

---
+++ JavaScript/Arrays/basic.js
@@ -0,0 +1,20 @@
+const todos = [
+  "Finish homework",
+  "Buy groceries",
+  "Walk the dog",
+  "Write code",
+  "Exercise",
+];
+
+// Lets learn some array methods
+
+// Log the numbe of todos
+console.log(`You have ${todos.length} todos left!`);
+// log the first todo
+console.log(todos[0]);
+
+// log the last todo
+console.log(todos[todos.length - 1]);
+
+// log the second last todo
+console.log(todos[todos.length - 2]);
