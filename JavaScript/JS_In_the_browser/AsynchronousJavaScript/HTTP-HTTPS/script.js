5a9d835eac3fd1aa0a4688e9e329a1283f664608
Nikola0707 <ristoski.nikola@gmail.com>
2024-06-18 21:01:26 +0200

    async lessons

--- JavaScript/JS_In_the_browser/AsynchronousJavaScript/HTTP-HTTPS/script.js
+++ JavaScript/JS_In_the_browser/AsynchronousJavaScript/HTTP-HTTPS/script.js
@@ -7,11 +7,32 @@ const fetchPostsButton = document.querySelector("#fetch-posts-data");
 
 async function fetchUsers() {
   try {
+    // Await the fetch call to get the response
     const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    //  Await the conversion of the response to JSON Format.
     const users = await response.json();
+    // Log the users data to the console
     console.log(users);
   } catch (error) {
     console.error("Error fetching user data", error);
   }
 }
 fetchUsersButton.addEventListener("click", () => fetchUsers());
+
+// Example = Fetching Data from an API using the then method (promises chaining)
+
+const fetchPostsURL = "https://jsonplaceholder.typicode.com/posts";
+async function fetchPosts() {
+  fetch(fetchPostsURL)
+    .then((response) => {
+      return response.json();
+    })
+    .then((posts) => {
+      console.log(posts);
+    })
+    .catch((error) => {
+      console.error("Error fetching post data:", error);
+    });
+}
+
+fetchPostsButton.addEventListener("click", () => fetchPosts());
---
+++ JavaScript/JS_In_the_browser/AsynchronousJavaScript/fetchingAndRenderingData/script.js
@@ -0,0 +1,34 @@
+const fetchPostsURL = "https://jsonplaceholder.typicode.com/posts";
+
+async function getPosts() {
+  const response = await fetch(fetchPostsURL);
+  const posts = await response.json();
+  return posts;
+}
+function renderPost(post) {
+  const section = document.querySelector(".posts");
+  const postDiv = document.createElement("div");
+
+  const postTitle = document.createElement("h2");
+  postTitle.innerText = post.title;
+
+  const postAuthor = document.createElement("h4");
+  postAuthor.innerText = `Author ID: ${post.userId}`;
+
+  const postBody = document.createElement("div");
+  postBody.innerText = post.body;
+
+  postDiv.appendChild(postTitle);
+  postDiv.appendChild(postAuthor);
+  postDiv.appendChild(postBody);
+
+  postDiv.classList.add("post");
+
+  section.appendChild(postDiv);
+}
+
+async function renderPosts() {
+  const response = await getPosts();
+  response.forEach((post) => renderPost(post));
+}
+renderPosts();
